c7e94bc350e1ced8043de30451cf7f1bad30de4a
at-if911 <2024i44.ise@epcet.edu.in>
2026-07-01 03:06:48 +0530

    added signin&signup

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const path = require("path");
 const express = require("express");
 
+const mongoose = require("mongoose");
+
+const UserRoutes = require("./routes/user")
+
 const app = express();
 const PORT = 9111;
+
+mongoose
+    .connect("mongodb://127.0.0.1:27017/bloggify")
+    .then(() => console.log("MONGODB CONNECTED"));
+
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
+app.use(express.urlencoded({ extended: false }));
+
 app.get("/", (req, res) => {
   res.render("home");  
 });
+
+app.use("/user", UserRoutes);
+
 
 app.listen(PORT, () => console.log("SERVER STARTED AT ", PORT));
---
+++ models/user.js
@@ -0,0 +1,71 @@
+const { createHmac, randomBytes } = require("crypto");
+
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    salt: {
+      type: String,
+    },
+    password: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    profileImageUrl: {
+      type: String,
+      default: "../public/images/default.jpg",
+    },
+    role: {
+      type: String,
+      enum: ["USER", "ADMIN"],
+      default: "USER",
+    },
+  },
+  { timestamps: true },
+);
+
+userSchema.pre("save", function (next) {
+  const user = this;
+
+  if (!user.isModified("password")) return;
+
+  const salt = randomBytes(16).toString();
+  const hashedPassword = createHmac("sha256", salt)
+    .update(user.password)
+    .digest("hex");
+
+  this.salt = salt;
+  this.password = hashedPassword;
+});
+
+userSchema.static("matchPassword", async function (email, password) {
+    const user = await this.findOne({ email });
+    
+  if (!user) throw new Error("user not found ");
+
+  const salt = user.salt;
+  const hashedpassword = user.password;
+
+  const userProvidedHash = createHmac("sha256", salt)
+    .update(password)
+    .digest("hex");
+
+  if (hashedpassword !== userProvidedHash)
+    throw new Error("Incorrect Password");
+
+  return user;
+});
+
+const User = model("user", userSchema);
+
+module.exports = User;
